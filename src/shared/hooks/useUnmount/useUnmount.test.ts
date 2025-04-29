9a74a86d44b19d6f594f9389b0856b4972635065
jihostudy <cale.jiho@gmail.com>
2025-04-29 20:29:35 +0900

    feat: useUnmount 커스텀 훅

---
+++ src/shared/hooks/useUnmount/useUnmount.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it, vi } from 'vitest'
+
+import { renderHook } from '@testing-library/react'
+
+import { useUnmount } from './useUnmount'
+
+describe('useUnmount', () => {
+  it('컴포넌트 언마운트 시 콜백이 호출되어야 한다', () => {
+    const callback = vi.fn()
+
+    const { unmount } = renderHook(() => useUnmount(callback))
+
+    // 컴포넌트를 언마운트 시키기
+    unmount()
+
+    expect(callback).toHaveBeenCalledTimes(1)
+  })
+})
