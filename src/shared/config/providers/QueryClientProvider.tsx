50984baebe3073fb4b1c7f55921733acb30167d6
jihostudy <cale.jiho@gmail.com>
2025-05-05 17:35:45 +0900

    feat: tanstack query devtools provider 추가

--- src/shared/config/providers/QueryClientProvider.tsx
+++ src/shared/config/providers/QueryClientProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
-
 import { ReactNode } from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { queryClient } from '../tanstack-query/queryClient'
 
@@ -10,5 +10,10 @@ interface Props {
 }
 
 export default function TanstackQueryProvider({ children }: Props) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
