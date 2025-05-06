3c52a36937f7988149bcaece005bb9d033c888a7
jihostudy <cale.jiho@gmail.com>
2025-05-06 23:41:18 +0900

    feat: 챌린지 상수를 양방향 편한 변환을 위해 구조 변경

--- src/entities/challenge/constant.ts
+++ src/entities/challenge/constant.ts
@@ -1,3 +1,5 @@
+import { ChallengeCategoryType, ChallengeCategoryTypeKor, ConstantPair, DayType, DayTypeKor } from './type'
+
 /** 챌린지 참여 여부 */
 export const CHALLENGE_PARTICIPATION_STATUS = ['NOT_STARTED', 'ONGOING', 'COMPLETED']
 
@@ -16,18 +18,49 @@ export const CHALLENGE_VERIFICATION_STATUS = [
 ] as const
 
 /** 요일 */
-export const DAYS = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'] as const
-export const KOR_DAYS = ['일', '월', '화', '수', '목', '금', '토'] as const
+export const DAY_PAIRS: ConstantPair[] = [
+  { kor: '일', eng: 'SUNDAY' },
+  { kor: '월', eng: 'MONDAY' },
+  { kor: '화', eng: 'TUESDAY' },
+  { kor: '수', eng: 'WEDNESDAY' },
+  { kor: '목', eng: 'THURSDAY' },
+  { kor: '금', eng: 'FRIDAY' },
+  { kor: '토', eng: 'SATURDAY' },
+] as const
+
+export const DAYS: DayType[] = DAY_PAIRS.map(pair => pair.eng)
+export const DAYS_KOR: DayTypeKor[] = DAY_PAIRS.map(pair => pair.kor)
 
 /** 카테고리 */
-export const CHALLENGE_CATEGORIES = [
-  'ZERO_WASTE',
-  'PLOGGING',
-  'CARBON_FOOTPRINT',
-  'ENERGY_SAVING',
-  'UPCYCLING',
-  'MEDIA',
-  'DIGITAL_CARBON',
-  'VEGAN',
-  'ETC',
+export const CHALLENGE_CATEGORY_PAIRS: ConstantPair[] = [
+  { kor: '제로 웨이스트', eng: 'ZERO_WASTE' },
+  { kor: '플로깅', eng: 'PLOGGING' },
+  { kor: '탄소 발자국', eng: 'CARBON_FOOTPRINT' },
+  { kor: '에너지 절약', eng: 'ENERGY_SAVING' },
+  { kor: '업사이클링', eng: 'UPCYCLING' },
+  { kor: '미디어', eng: 'MEDIA' },
+  { kor: '디지털 탄소', eng: 'DIGITAL_CARBON' },
+  { kor: '비건', eng: 'VEGAN' },
+  { kor: '기타', eng: 'ETC' },
 ] as const
+
+export const CHALLENGE_CATEGORIES: ChallengeCategoryType[] = CHALLENGE_CATEGORY_PAIRS.map(pair => pair.eng)
+export const CHALLENGE_CATEGORIES_KOR: ChallengeCategoryTypeKor[] = CHALLENGE_CATEGORY_PAIRS.map(pair => pair.kor)
+
+/**
+ * 한글을 영어로, 영어를 한글로 바꿔주는 함수
+ * 예시(영어 to 한글): convertLanguage(CHALLENGE_CATEGORY_PAIRS, 'eng', 'kor')
+ */
+export const convertLanguage = <
+  T extends readonly Record<string, string>[],
+  FromKey extends keyof T[number],
+  ToKey extends keyof T[number],
+>(
+  pairs: T,
+  fromKey: FromKey,
+  toKey: ToKey,
+) => {
+  return (fromValue: T[number][FromKey]): T[number][ToKey] | undefined => {
+    return (pairs.find((pair: T[number]) => pair[fromKey] === fromValue) as T[number] | undefined)?.[toKey]
+  }
+}
