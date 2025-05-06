21d0ba607730aa7f09f7054c3c3cf53879b5378f
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-05-06 17:21:49 +0900

    feat: Chakra Provider
    Chakra 사용을 위한 세팅

--- src/shared/config/providers/Providers.tsx
+++ src/shared/config/providers/Providers.tsx
@@ -3,11 +3,12 @@
 import { ReactNode } from 'react'
 
 import { EmotionCacheProvider } from './EmotionCacheProvider'
+import { ChakraUseProvider } from './ChakraProvider'
 
 /**
  * 전역 Layout.tsx에 지정되는 Provider를 모아두는 저장소입니다.
  * 동일 경로에 Provider를 생성 후, 적용해주세요
  */
 export function Providers({ children }: { children: ReactNode }) {
-  return <EmotionCacheProvider>{children}</EmotionCacheProvider>
+  return <EmotionCacheProvider><ChakraUseProvider>{children}</ChakraUseProvider></EmotionCacheProvider>
 }
