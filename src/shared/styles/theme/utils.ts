3d089fc58b6f2d98150ea1a01762060c025a9cc6
jihostudy <cale.jiho@gmail.com>
2025-05-07 11:19:01 +0900

    feat: theme fontSize utils added

--- src/shared/styles/theme/utils.ts
+++ src/shared/styles/theme/utils.ts
@@ -1,7 +1,12 @@
 import { theme } from '.'
-import { ThemeColorType } from './type'
+import { ThemeColorType, ThemeFontSizeType } from './type'
 
 export const getThemeColor = (key?: ThemeColorType) => {
   if (!key) return undefined
   return theme.colors[key]?.base
+}
+
+export const getThemeFontSize = (key?: ThemeFontSizeType) => {
+  if (!key) return undefined
+  return theme.fontSize[key]
 }
