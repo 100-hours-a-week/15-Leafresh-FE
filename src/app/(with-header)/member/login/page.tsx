df1c9f675c9dbe68315010100f55f1fbc652d846
jihostudy <cale.jiho@gmail.com>
2025-05-07 23:45:35 +0900

    feat: 카카오 로그인 페이지 구현

--- src/app/(with-header)/member/login/page.tsx
+++ src/app/(with-header)/member/login/page.tsx
@@ -2,15 +2,42 @@
 
 import Image from 'next/image'
 
+import { useRef } from 'react'
 import styled from '@emotion/styled'
+import { useQuery } from '@tanstack/react-query'
 
+import { LowercaseOAuthType } from '@entities/member/type'
+import { Login } from '@features/member/api/oauth-login'
+import Loading from '@shared/components/loading'
+import { QUERY_KEYS } from '@shared/constants/tanstack-query/query-keys'
+import { ToastType } from '@shared/context/Toast/type'
+import { useToast } from '@shared/hooks/useToast/useToast'
 import { theme } from '@shared/styles/theme'
 import KakaoLoginButton from '@public/image/kakao_login.svg'
 import LogoImage from '@public/image/logo.svg'
 
 const LoginPage = () => {
-  const loginHandler = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`
+  const openToast = useToast()
+
+  const providerRef = useRef<LowercaseOAuthType>('kakao')
+
+  const { refetch: StartLogin, isLoading } = useQuery({
+    queryKey: QUERY_KEYS.MEMBER.AUTH.LOGIN(providerRef.current),
+    queryFn: () => Login(providerRef.current),
+    enabled: false,
+  })
+
+  const handleLogin = async (provider: LowercaseOAuthType) => {
+    providerRef.current = provider // 현재 provider 갱신
+    try {
+      const { data } = await StartLogin()
+      if (data?.data) {
+        const { redirectUrl } = data.data
+        window.location.href = redirectUrl
+      }
+    } catch (_error) {
+      openToast(ToastType.Error, `${provider} 로그인 실패\n재시도 해주세요`)
+    }
   }
 
   return (
@@ -21,7 +48,11 @@ const LoginPage = () => {
         <Text>로그인 / 회원가입</Text>
         <Line />
       </DividerWrapper>
-      <KakaoImage src={KakaoLoginButton} alt='kakao' onClick={loginHandler} />
+      {!isLoading ? (
+        <KakaoImage src={KakaoLoginButton} alt='kakao' onClick={() => handleLogin('kakao')} />
+      ) : (
+        <Loading />
+      )}
     </Container>
   )
 }
@@ -66,5 +97,5 @@ const Text = styled.div`
 
 const KakaoImage = styled(Image)`
   width: 100%;
-  height: 50px;
+  height: 60px;
 `
