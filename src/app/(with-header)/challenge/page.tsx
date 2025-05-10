2cbb7dcdd0889c0a209b927f378133359a5237a6
jihostudy <cale.jiho@gmail.com>
2025-05-11 02:47:10 +0900

    chore: 콘솔 로그 삭제

--- src/app/(with-header)/challenge/page.tsx
+++ src/app/(with-header)/challenge/page.tsx
@@ -11,42 +11,28 @@ import { getDayOfWeek } from '@shared/lib/date/utils'
 
 const Page = async () => {
   try {
-    console.log('[SSR] 시작')
-
     const queryClient = getQueryClient()
     const now = new Date()
     const dayOfWeek: DayType = getDayOfWeek(now)
 
-    console.log('[SSR] 계산된 dayOfWeek:', dayOfWeek)
-
     await Promise.all([
-      queryClient
-        .prefetchQuery({
-          queryKey: QUERY_KEYS.CHALLENGE.GROUP.CATEGORIES,
-          queryFn: getGroupChallengeCategoryList,
-        })
-        .then(() => console.log('[SSR] 단체 카테고리 성공'))
-        .catch(err => console.error('[SSR] 단체 카테고리 에러', err)),
+      queryClient.prefetchQuery({
+        queryKey: QUERY_KEYS.CHALLENGE.GROUP.CATEGORIES,
+        queryFn: getGroupChallengeCategoryList,
+      }),
 
-      queryClient
-        .prefetchQuery({
-          queryKey: QUERY_KEYS.CHALLENGE.EVENT.LIST,
-          queryFn: getEventChallengeList,
-        })
-        .then(() => console.log('[SSR] 이벤트 챌린지 성공'))
-        .catch(err => console.error('[SSR] 이벤트 챌린지 에러', err)),
+      queryClient.prefetchQuery({
+        queryKey: QUERY_KEYS.CHALLENGE.EVENT.LIST,
+        queryFn: getEventChallengeList,
+      }),
 
-      queryClient
-        .prefetchQuery({
-          queryKey: QUERY_KEYS.CHALLENGE.PERSONAL.LIST(dayOfWeek),
-          queryFn: () => getPersonalChallengeList({ dayOfWeek }),
-        })
-        .then(() => console.log('[SSR] 개인 챌린지 성공'))
-        .catch(err => console.error('[SSR] 개인 챌린지 에러', err)),
+      queryClient.prefetchQuery({
+        queryKey: QUERY_KEYS.CHALLENGE.PERSONAL.LIST(dayOfWeek),
+        queryFn: () => getPersonalChallengeList({ dayOfWeek }),
+      }),
     ])
 
     const dehydratedState = dehydrate(queryClient)
-    console.log('[SSR] Dehydrated 완료')
 
     return (
       <HydrationBoundary state={dehydratedState}>
