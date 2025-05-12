4a28af3e373fb210aabceeb4689e01c96ad66e7a
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-05-12 23:28:40 +0900

    feat: 메인 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
 import { ReactNode } from 'react'
 
+import Header from '@shared/components/header'
+import Footer from '@shared/components/footer'
+import HomePage from '@features/main/HomePage'
+import { HydrationBoundary } from '@tanstack/react-query'
+
 interface MainPageProps {
   className?: string
 }
 
+const HEADER_HEIGHT = 60
+const WIDTH_PADDING = 35
+
 const MainPage = ({ className }: MainPageProps): ReactNode => {
-  return <div className={className}>MainPage</div>
+  return (
+    <HydrationBoundary>
+      <>
+        <Header height={HEADER_HEIGHT} padding={WIDTH_PADDING} />
+        <main>
+          <HomePage />
+        </main>
+        <Footer padding={WIDTH_PADDING} />
+      </>
+    </HydrationBoundary>
+  )
 }
 
 export default MainPage
