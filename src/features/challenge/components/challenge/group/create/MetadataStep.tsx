8bcc4472b9e81c002827d7e15496c47334e2e363
jihostudy <cale.jiho@gmail.com>
2025-05-21 22:40:25 +0900

    fix : 이미지 순서 변경 오류 (동일 key 설정 오류)

--- src/features/challenge/components/challenge/group/create/MetadataStep.tsx
+++ src/features/challenge/components/challenge/group/create/MetadataStep.tsx
@@ -119,10 +119,6 @@ const MetaDataStep = ({ form, onNext }: MetaDataStepProps) => {
   )
 
   const handleMetaSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log('클릭 태그:', (e.target as HTMLElement).tagName)
-    console.log('클래스명:', (e.target as HTMLElement).className)
-    console.log('텍스트:', (e.target as HTMLElement).textContent)
-
     e.preventDefault()
     if (!isSubmitted) {
       setIsSubmitted(true)
