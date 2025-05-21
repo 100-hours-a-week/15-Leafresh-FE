75a9c542aa69a03baecfec21172bbb81f898c734
jihostudy <cale.jiho@gmail.com>
2025-05-21 22:26:41 +0900

    style: 단체 챌린지 상세 행간격 늘리기

--- src/features/challenge/components/challenge/group/details/ChallengeGroupDetails.tsx
+++ src/features/challenge/components/challenge/group/details/ChallengeGroupDetails.tsx
@@ -54,7 +54,7 @@ interface ChallengeGroupDetailsProps {
 }
 
 const ChallengeGroupDetails = ({ challengeId, className }: ChallengeGroupDetailsProps) => {
-  const { userInfo, clearUserInfo } = useOAuthUserStore()
+  const { userInfo } = useOAuthUserStore()
   const { openConfirmModal } = useConfirmModalStore()
 
   const isLoggedIn: boolean = userInfo && userInfo.isMember ? true : false
@@ -174,7 +174,7 @@ const ChallengeGroupDetails = ({ challengeId, className }: ChallengeGroupDetails
         <StyledBackButton onClick={() => router.push(URL.CHALLENGE.GROUP.LIST.value(category))} />
         <Thumbnail src={thumbnailUrl} alt='썸네일' width={500} height={200} />
         <Participant>
-          <LucideIcon name='UserRound' size={15} fill='lfBlack' /> {currentParticipantCount}명 참여중
+          <LucideIcon name='UsersRound' size={24} color='lfBlue' /> {currentParticipantCount}명 참여중
         </Participant>
 
         <Descriptions>
@@ -294,17 +294,17 @@ const Participant = styled.div`
   padding: 14px 0;
   font-size: ${theme.fontSize.sm};
   font-weight: ${theme.fontWeight.medium};
-  color: ${theme.colors.lfBlack.base};
+  color: ${theme.colors.lfBlue.base};
 
   display: flex;
   align-items: center;
-  gap: 4px;
+  gap: 5px;
 `
 
 const Descriptions = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 12px;
+  gap: 16px;
 `
 
 const Title = styled.h2`
@@ -315,6 +315,7 @@ const Description = styled.p`
   font-size: ${theme.fontSize.base};
   white-space: pre-wrap;
   word-break: break-word;
+  line-height: 1.6;
 `
 
 const MoreButton = styled.button`
@@ -424,7 +425,7 @@ const StyledBackButton = styled(BackButton)`
 
 const NoVerficiationImageText = styled.div`
   text-align: center;
-  padding: 20px;
+  padding: 30px;
   font-weight: ${theme.fontWeight.medium};
   color: ${theme.colors.lfRed.base};
 `
