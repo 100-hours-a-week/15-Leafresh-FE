0031b5efa6fd5a1e1d7648d5c9f90c22d9530bf0
jihostudy <cale.jiho@gmail.com>
2025-06-02 14:22:18 +0900

    feat: 현재경로인지 판단 로직

--- src/shared/components/navbar/navbar.tsx
+++ src/shared/components/navbar/navbar.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 import { ReactNode } from 'react'
 import styled from '@emotion/styled'
 
+import { URL } from '@shared/constants/route/route'
 import { useAuth } from '@shared/hooks/useAuth/useAuth'
 import LucideIcon from '@shared/lib/ui/LucideIcon'
 import { theme } from '@shared/styles/theme'
@@ -13,16 +14,36 @@ import { NAVBAR_TABS } from './tabs'
 
 export const Navbar = (): ReactNode => {
   const router = useRouter()
+  const pathname = usePathname()
+
   const { isLoggedIn } = useAuth()
+
+  // 현재 경로가 어떤 탭인지 판단
+  const isCurrentTab = (label: string) => {
+    if (label === '홈') return pathname === URL.MAIN.INDEX.value
+    if (label === '챌린지') return pathname.startsWith('/challenge') && !pathname.startsWith('/challenge/participate')
+    if (label === '인증') return pathname.startsWith('/challenge/participate')
+    if (label === '상점') return pathname.startsWith('/store')
+    if (label === '마이페이지') return pathname.startsWith('/member')
+    return false
+  }
 
   return (
     <NavbarWrapper>
-      {NAVBAR_TABS.map(({ label, icon, href }) => (
-        <NavButton key={label} onClick={() => router.push(href)}>
-          <LucideIcon name={icon} size={24} />
-          <Label>{label}</Label>
-        </NavButton>
-      ))}
+      {NAVBAR_TABS.map(({ label, icon, href }) => {
+        const isActive: boolean = isCurrentTab(label)
+        return (
+          <NavButton key={label} onClick={() => router.push(href)}>
+            <LucideIcon
+              name={icon}
+              size={24}
+              color={isActive ? 'lfBlack' : 'lfGray'}
+              strokeWidth={isActive ? 2.2 : 2}
+            />
+            <Label active={isActive}>{label}</Label>
+          </NavButton>
+        )
+      })}
     </NavbarWrapper>
   )
 }
@@ -59,16 +80,11 @@ const NavButton = styled.button`
   background: none;
   border: none;
   cursor: pointer;
-
-  color: ${theme.colors.lfDarkGray.base};
-
-  &:hover {
-    color: ${theme.colors.lfGreenMain.base};
-  }
 `
 
-const Label = styled.span`
+const Label = styled.span<{ active: boolean }>`
   font-size: ${theme.fontSize.sm};
   font-weight: ${theme.fontWeight.medium};
   margin-top: 4px;
+  color: ${({ active }) => (active ? theme.colors.lfBlack.base : theme.colors.lfGray.base)};
 `
