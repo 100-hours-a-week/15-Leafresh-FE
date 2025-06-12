08b34917939df6f59abe6eaa784a45f1827c5485
jihostudy <cale.jiho@gmail.com>
2025-06-12 14:30:25 +0900

    fix: 서버 요청 삭제 후 테스트

--- src/app/(with-layout)/(with-header)/page.tsx
+++ src/app/(with-layout)/(with-header)/page.tsx
@@ -1,11 +1,6 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { dehydrate } from '@tanstack/react-query'
 
 import { DayType } from '@entities/challenge/type'
-import { getEventChallengeList } from '@features/challenge/api/get-event-challenge-list'
-import { getGroupChallengeCategoryList } from '@features/challenge/api/get-group-challenge-categories'
-import { getPersonalChallengeList } from '@features/challenge/api/get-personal-challenge-list'
-import { QUERY_OPTIONS } from '@shared/config/tanstack-query/query-defaults'
-import { QUERY_KEYS } from '@shared/config/tanstack-query/query-keys'
 import { getQueryClient } from '@shared/config/tanstack-query/queryClient'
 import { getDayOfWeek } from '@shared/lib/date/utils'
 
@@ -17,32 +12,32 @@ const Page = async () => {
     const now = new Date()
     const dayOfWeek: DayType = getDayOfWeek(now)
 
-    await Promise.all([
-      queryClient.prefetchQuery({
-        queryKey: QUERY_KEYS.CHALLENGE.GROUP.CATEGORIES,
-        queryFn: getGroupChallengeCategoryList,
-        ...QUERY_OPTIONS.CHALLENGE.GROUP.CATEGORIES,
-      }),
+    // await Promise.all([
+    //   queryClient.prefetchQuery({
+    //     queryKey: QUERY_KEYS.CHALLENGE.GROUP.CATEGORIES,
+    //     queryFn: getGroupChallengeCategoryList,
+    //     ...QUERY_OPTIONS.CHALLENGE.GROUP.CATEGORIES,
+    //   }),
 
-      queryClient.prefetchQuery({
-        queryKey: QUERY_KEYS.CHALLENGE.EVENT.LIST,
-        queryFn: getEventChallengeList,
-        ...QUERY_OPTIONS.CHALLENGE.EVENT.LIST,
-      }),
+    //   queryClient.prefetchQuery({
+    //     queryKey: QUERY_KEYS.CHALLENGE.EVENT.LIST,
+    //     queryFn: getEventChallengeList,
+    //     ...QUERY_OPTIONS.CHALLENGE.EVENT.LIST,
+    //   }),
 
-      queryClient.prefetchQuery({
-        queryKey: QUERY_KEYS.CHALLENGE.PERSONAL.LIST(dayOfWeek),
-        queryFn: () => getPersonalChallengeList({ dayOfWeek }),
-        ...QUERY_OPTIONS.CHALLENGE.PERSONAL.LIST,
-      }),
-    ])
+    //   queryClient.prefetchQuery({
+    //     queryKey: QUERY_KEYS.CHALLENGE.PERSONAL.LIST(dayOfWeek),
+    //     queryFn: () => getPersonalChallengeList({ dayOfWeek }),
+    //     ...QUERY_OPTIONS.CHALLENGE.PERSONAL.LIST,
+    //   }),
+    // ])
 
     const dehydratedState = dehydrate(queryClient)
 
     return (
-      <HydrationBoundary state={dehydratedState}>
-        <MainPage />
-      </HydrationBoundary>
+      // <HydrationBoundary state={dehydratedState}>
+      <MainPage />
+      // </HydrationBoundary>
     )
   } catch (err) {
     console.error('[SSR] 전체 실패', err)
