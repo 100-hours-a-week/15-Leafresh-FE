f1401acec92b6466e838a06b44dd46d3ba487b1e
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-06-16 13:43:42 +0900

    feat: query option으로 수정

--- src/app/(with-layout)/(with-header)/member/profile/badge/page.tsx
+++ src/app/(with-layout)/(with-header)/member/profile/badge/page.tsx
@@ -2,6 +2,7 @@ import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 
 import { getBadgeList } from '@features/member/api/profile/get-badge'
 import BadgePage from '@features/member/components/member/profile/badge'
+import { QUERY_OPTIONS } from '@shared/config/tanstack-query/query-defaults'
 import { QUERY_KEYS } from '@shared/config/tanstack-query/query-keys'
 import { getQueryClient } from '@shared/config/tanstack-query/queryClient'
 
@@ -12,7 +13,7 @@ const Page = async () => {
     await queryClient.prefetchQuery({
       queryKey: QUERY_KEYS.MEMBER.BADGES.LIST,
       queryFn: getBadgeList,
-      ...QUERY_KEYS.MEMBER.BADGES.LIST,
+      ...QUERY_OPTIONS.MEMBER.BADGES.LIST,
     })
 
     const dehydratedState = dehydrate(queryClient)
