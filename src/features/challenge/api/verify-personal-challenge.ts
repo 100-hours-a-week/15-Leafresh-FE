5a7f507bbe898778ca24685c3023afd2696b3af6
jihostudy <cale.jiho@gmail.com>
2025-06-16 13:01:58 +0900

    feat: 개인 챌린지 롱폴링 API 추가

--- src/features/challenge/api/verify-personal-challenge.ts
+++ src/features/challenge/api/verify-personal-challenge.ts
@@ -1,3 +1,4 @@
+import { ChallengeVerificationStatusType } from '@entities/challenge/type'
 import { ENDPOINTS } from '@shared/constants/endpoint/endpoint'
 import { fetchRequest } from '@shared/lib/api'
 import { ISOFormatString } from '@shared/types/date'
@@ -22,4 +23,15 @@ export const VerifyGroupChallenge = ({ challengeId, body }: VerifyVariables) => 
   return fetchRequest<VerifyGroupChallengeResponse>(ENDPOINTS.CHALLENGE.PERSONAL.VERIFY(challengeId), {
     body,
   })
+}
+
+export type PersonalChallengeVerificationResultResponse = {
+  status: ChallengeVerificationStatusType
+}
+
+/** 개인 챌린지 인증 결과 조회 (롱폴링) */
+export const PersonalChallengeVerificationResult = (personalChallengeId: number) => {
+  return fetchRequest<PersonalChallengeVerificationResultResponse>(
+    ENDPOINTS.CHALLENGE.PERSONAL.VERIFICATION_RESULT(personalChallengeId),
+  )
 }
