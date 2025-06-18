2be986bf52832628f03c783f9c6ec99a81062506
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-06-18 17:44:46 +0900

    feat: 롱폴링과 api 호출 로직 분리

---
+++ src/features/member/hooks/useFeedbackPolling.ts
@@ -0,0 +1,20 @@
+// src/features/member/hooks/useFeedbackPolling.ts
+import { useQuery } from '@tanstack/react-query'
+import { QUERY_KEYS } from '@shared/config/tanstack-query/query-keys'
+import { QUERY_OPTIONS } from '@shared/config/tanstack-query/query-defaults'
+import { getFeedbackResult } from '../api/profile/get-member-feedback-result'
+
+type PollingQueryOptions = {
+  enabled: boolean
+}
+
+export const useFeedbackPolling = ({ enabled = false }: PollingQueryOptions) => {
+  return useQuery({
+    queryKey: QUERY_KEYS.MEMBER.FEEDBACK.GET_FEEDBACK,
+    queryFn: () => getFeedbackResult(),
+    ...QUERY_OPTIONS.MEMBER.FEEDBACK,
+    enabled,
+    refetchInterval: 3000, // 3초마다
+    refetchIntervalInBackground: true,
+  })
+}
