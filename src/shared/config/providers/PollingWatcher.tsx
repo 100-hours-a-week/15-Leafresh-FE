95b8a869841dcbc72c47a94b7b13618327dce7b1
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-06-18 17:44:27 +0900

    feat: Watcher에 피드백 로직 추가

--- src/shared/config/providers/PollingWatcher.tsx
+++ src/shared/config/providers/PollingWatcher.tsx
@@ -2,6 +2,7 @@
 
 import { useGroupChallengeVerificationResult } from '@features/challenge/hook/use-group-verification-result'
 import { usePersonalChallengeVerificationResult } from '@features/challenge/hook/use-personal-verification-result'
+import { useFeedbackPolling } from '@features/member/hooks/useFeedbackPolling'
 import { usePollingStore } from '@shared/context/polling/PollingStore'
 import { ToastType } from '@shared/context/toast/type'
 import { useToast } from '@shared/hooks/useToast/useToast'
@@ -14,12 +15,13 @@ enum PollingTarget {
 
 const PollingWatcher = () => {
   const openToast = useToast()
-  const { polling, removePersonalChallengeId, removeGroupChallengeId } = usePollingStore()
+  const { polling, removePersonalChallengeId, removeGroupChallengeId, clearFeedbackPolling } = usePollingStore()
 
   const { challenge, member } = polling
 
   const personalChallengeIdList: number[] = challenge.verification.personalChallengeIdList // 개인 챌린지 롱폴링ID
   const groupChallengeIdList: number[] = challenge.verification.groupChallengeIdList // 단체 챌린지 롱폴링ID
+  const feedbackWhether: boolean = member.feedback
 
   /** 이벤트 핸들러 */
   // TODO: type=단체 챌린지 인증 제출 핸들링
@@ -34,6 +36,13 @@ const PollingWatcher = () => {
     if (type === PollingTarget.GROUP_CHALLENGE_VERIFICATION_RESULT) {
       removeGroupChallengeId(challengeId) // 챌린지 제거
       openToast(ToastType.Success, `단체 챌린지 인증 결과 도착!\n알림창을 확인해주세요`)
+    }
+  }
+  // 피드백 요청
+  const handleCompleteFeedback = (type: PollingTarget) => {
+    if (type === PollingTarget.FEEDBACK) {
+      clearFeedbackPolling()
+      openToast(ToastType.Success, `피드백 도착!\n마이페이지를 확인해주세요`)
     }
   }
   return (
@@ -55,6 +64,8 @@ const PollingWatcher = () => {
           onComplete={handleOnCompleteChallenge}
         />
       ))}
+
+      {feedbackWhether && <PollingFeedbackResult type={PollingTarget.FEEDBACK} onComplete={handleCompleteFeedback} />}
     </>
   )
 }
@@ -85,6 +96,26 @@ const PollingChallengeResult = ({ type, challengeId, onComplete }: PollingChalle
     }
   }
 
+  return null
+}
+
+interface FeedbackResultProps {
+  type: PollingTarget
+  onComplete: (type: PollingTarget) => void
+}
+const PollingFeedbackResult = ({ type, onComplete }: FeedbackResultProps) => {
+  const FeedbackQuery = useFeedbackPolling({
+    enabled: type === PollingTarget.FEEDBACK,
+  })
+
+  const data = FeedbackQuery.data
+
+  if (data) {
+    const { data: result } = data
+    if (result !== null) {
+      onComplete(type)
+    }
+  }
   return null
 }
 
