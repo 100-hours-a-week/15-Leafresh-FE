8e389bdad1141b184f8c7103b5656ebebd839263
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-06-23 17:45:30 +0900

    chore: eslint 규칙 적용

--- src/widgets/challenge/group/[id]/verification/details/challenge-group-verification-details.tsx
+++ src/widgets/challenge/group/[id]/verification/details/challenge-group-verification-details.tsx
@@ -3,10 +3,12 @@ import { ReactNode, useEffect, useState } from 'react'
 
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+
 import styled from '@emotion/styled'
 import { useQuery } from '@tanstack/react-query'
 
 import { CommentList } from '@/features/challenge/components'
+
 import {
   CreateVerificationLikeResponse,
   CreateVerificationLikeVariables,
