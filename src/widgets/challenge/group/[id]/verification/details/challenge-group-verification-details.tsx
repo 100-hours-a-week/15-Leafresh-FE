b88ac4568a1f3af150cc0bb6640bb73a255a1ac5
jihostudy <cale.jiho@gmail.com>
2025-06-23 23:54:24 +0900

    fix: 인증 상세 조회수 중복 증가 방지

--- src/widgets/challenge/group/[id]/verification/details/challenge-group-verification-details.tsx
+++ src/widgets/challenge/group/[id]/verification/details/challenge-group-verification-details.tsx
@@ -60,9 +60,9 @@ export const VerificationDetails = ({
     queryKey: QUERY_KEYS.CHALLENGE.GROUP.VERIFICATION.DETAILS(challengeId, verificationId),
     queryFn: () => getVerificationDetails({ challengeId, verificationId }),
     ...QUERY_OPTIONS.CHALLENGE.GROUP.VERIFICATION.DETAILS,
-    enabled: isClient,
-    // refetchOnMount: false,
-    // refetchOnWindowFocus: false,
+    // enabled: isClient,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
   })
 
   const { data: commentData } = useQuery({
