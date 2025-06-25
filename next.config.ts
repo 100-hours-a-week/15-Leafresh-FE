9a104871348fdcdcfc8f06c9e6208afd03513609
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-06-25 15:45:43 +0900

    feat: 환경 별 이미지 호스트 네임 할당

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from 'next'
 
+const runtimeEnv = process.env.NEXT_PUBLIC_RUNTIME
+
+let gcsHostname: string
+
+switch (runtimeEnv) {
+  case 'local':
+    gcsHostname = 'leafresh-images'
+    break
+  case 'dev':
+    gcsHostname = 'leafresh-images'
+    break
+  case 'prod':
+    gcsHostname = 'leafresh-prod-images'
+  default:
+    throw new Error(`Unknown Image Route NEXT_PUBLIC_RUNTIME: ${runtimeEnv}`)
+}
+
 const nextConfig: NextConfig = {
   /* config options here d*/
   compiler: {
@@ -10,7 +27,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com', // 정확히 이 도메인이어야 함
-        pathname: '/leafresh-images/**', // 해당 경로 하위 모든 이미지 허용
+        pathname: `/${gcsHostname}/**`, // 해당 경로 하위 모든 이미지 허용
       },
       {
         protocol: 'http',
