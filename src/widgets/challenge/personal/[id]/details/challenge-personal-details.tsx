8208d8008af1b634b6d0561e88813995758db40c
sunsetdust <115306845+sunsetdust@users.noreply.github.com>
2025-07-08 04:24:36 +0900

    chore: loading 대신 priority 적용

--- src/widgets/challenge/personal/[id]/details/challenge-personal-details.tsx
+++ src/widgets/challenge/personal/[id]/details/challenge-personal-details.tsx
@@ -188,7 +188,7 @@ export const ChallengePersonalDetails = ({ challengeId, className }: ChallengePe
     <Wrapper className={className}>
       <DescriptionSection>
         <ThumbnailImageWrapper>
-          <Thumbnail src={thumbnailUrl} alt='썸네일' fill sizes='(max-width: 640px) 100vw, 400px' loading='eager' />
+          <Thumbnail src={thumbnailUrl} alt='썸네일' fill sizes='(max-width: 640px) 100vw, 400px' priority />
         </ThumbnailImageWrapper>
 
         <Descriptions>
