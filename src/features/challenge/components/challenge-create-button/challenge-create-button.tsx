9bd2fa7fbed7e78ec362a021b1ddad70e75c057e
jihostudy <cale.jiho@gmail.com>
2025-07-07 14:39:49 +0900

    chore: challengeCreateButton - 피드 페이지에서도 챌린지 생성 버튼 표시
    chore: 주석 제거

--- src/features/challenge/components/challenge-create-button/challenge-create-button.tsx
+++ src/features/challenge/components/challenge-create-button/challenge-create-button.tsx
@@ -32,8 +32,8 @@ export const ChallengeCreateButton = () => {
     router.push(URL.CHALLENGE.GROUP.CREATE.value())
   }
 
-  // TODO: 피드 페이지 생성되면 넣기
-  if (pathname !== URL.MAIN.INDEX.value && !pathname.startsWith('/feed')) return null
+  if (pathname !== URL.MAIN.INDEX.value && !pathname.startsWith('/challenge/group/feed')) return null
+
   return (
     <Container>
       <ButtonIcon name='Plus' color='lfWhite' size={24} onClick={handleCreateChallenge} />
