a8b76bafab6a21fd17e1fe9c1c31756295790b67
jihostudy <cale.jiho@gmail.com>
2025-07-08 22:17:33 +0900

    feat: 스타일 체크

--- src/shared/components/toast/styles.ts
+++ src/shared/components/toast/styles.ts
@@ -2,32 +2,6 @@ import { motion } from 'motion/react'
 
 import styled from '@emotion/styled'
 
-import { ToastType } from '@/shared/context'
-
-export const MotionContainer = styled(motion.div)<{ toastType: ToastType }>`
-  position: absolute;
-  bottom: 10px;
-  left: 50%;
-  transform: translateX(-50%);
-  z-index: 999;
-
-  display: flex;
-  align-items: center;
-  gap: 4px;
-
-  min-width: ${({ toastType }) => (toastType === 'Success' ? '209px' : '256px')};
-  height: 60px;
-  width: 250px;
-  padding: 0px 20px;
-
-  background-color: ${({ theme }) => theme.colors.lfWhite.base};
-  border-radius: ${({ theme }) => theme.radius.base};
-  box-shadow: ${({ theme }) => theme.shadow.lfInput};
-  color: ${({ toastType, theme }) => (toastType === 'Success' ? theme.colors.lfBlack.base : theme.colors.lfRed.base)};
-  font-size: ${({ theme }) => theme.fontSize.sm};
-  font-weight: ${({ theme }) => theme.fontWeight.medium};
-`
-
 export const Wrapper = styled.div<{ $toastCount: number; $isPaused: boolean }>`
   width: 250px;
   position: absolute;
