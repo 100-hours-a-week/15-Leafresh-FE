0bfa04bfe945729668cc667b8222ca2c6ee4e018
jihostudy <cale.jiho@gmail.com>
2025-07-12 16:46:59 +0900

    feat: URI 생성시 query + fragment 빌드 코드 생성 및 적용

--- src/shared/constants/route.ts
+++ src/shared/constants/route.ts
@@ -1,4 +1,7 @@
 import { ChallengeCategoryType } from '@/entities/challenge/model'
+import { ChallengeStatus } from '@/entities/member/api'
+
+import { buildURI } from '../lib'
 
 type URL = {
   name: string
@@ -14,40 +17,37 @@ const CHALLENGE_URL = {
   PERSONAL: {
     DETAILS: {
       name: '개인 챌린지 상세',
-      value: (personalId: number) => `/challenge/personal/${personalId}`,
-      dynamicPath: '/challenge/personal/[id]', // 👈 추가
+      value: (personalId: number) => buildURI(`/challenge/personal/${personalId}`),
+      dynamicPath: '/challenge/personal/[id]',
       isProtected: false,
       hasBackButton: true,
     },
   },
   GROUP: {
-    // TODO: 단체 챌린지 목록 페이지 제거하기
     LIST: {
       name: '단체 챌린지 목록',
-      value: (category?: ChallengeCategoryType) =>
-        category ? `/challenge/group/list?category=${category}` : '/challenge/group/list',
+      value: (category?: ChallengeCategoryType) => buildURI('/challenge/group/list', { query: { category } }),
       dynamicPath: '/challenge/group/list',
       isProtected: false,
       hasBackButton: true,
     },
     CREATE: {
       name: '단체 챌린지 생성',
-      value: (category?: ChallengeCategoryType) =>
-        category ? `/challenge/group/create?category=${category}` : `/challenge/group/create`,
+      value: (category?: ChallengeCategoryType) => buildURI('/challenge/group/create', { query: { category } }),
       dynamicPath: '/challenge/group/create',
       isProtected: true,
       hasBackButton: true,
     },
     MODIFY: {
       name: '단체 챌린지 수정',
-      value: (challengeId: number) => `/challenge/group/${challengeId}/modify`,
+      value: (challengeId: number) => buildURI(`/challenge/group/${challengeId}/modify`),
       dynamicPath: '/challenge/group/[challengeId]/modify',
       isProtected: true,
       hasBackButton: true,
     },
     DETAILS: {
       name: '단체 챌린지 상세',
-      value: (challengeId: number) => `/challenge/group/${challengeId}`,
+      value: (challengeId: number) => buildURI(`/challenge/group/${challengeId}`),
       dynamicPath: '/challenge/group/[challengeId]',
       isProtected: false,
       hasBackButton: true,
@@ -55,7 +55,7 @@ const CHALLENGE_URL = {
     VERIFICATION: {
       LIST: {
         name: '이용자 인증 내역 목록',
-        value: (challengeId: number) => `/challenge/group/${challengeId}/verification/list`,
+        value: (challengeId: number) => buildURI(`/challenge/group/${challengeId}/verification/list`),
         dynamicPath: '/challenge/group/[challengeId]/verification/list',
         isProtected: false,
         hasBackButton: true,
@@ -63,7 +63,7 @@ const CHALLENGE_URL = {
       DETAILS: {
         name: '챌린지 인증 상세',
         value: (challengeId: number, verificationId: number) =>
-          `/challenge/group/${challengeId}/verification/${verificationId}`,
+          buildURI(`/challenge/group/${challengeId}/verification/${verificationId}`),
         dynamicPath: '/challenge/group/[challengeId]/verification/[verificationId]',
         isProtected: false,
         hasBackButton: true,
@@ -71,7 +71,7 @@ const CHALLENGE_URL = {
     },
     FEED: {
       name: '챌린지 인증 피드',
-      value: `/challenge/group/feed`,
+      value: buildURI(`/challenge/group/feed`),
       isProtected: false,
       hasBackButton: false,
     },
@@ -81,7 +81,7 @@ const CHALLENGE_URL = {
 const MAIN_URL = {
   INDEX: {
     name: '메인',
-    value: '/',
+    value: buildURI('/'),
     isProtected: false,
     hasBackButton: false,
   } as URL,
@@ -90,75 +90,70 @@ const MAIN_URL = {
 const MEMBER_URL = {
   LOGIN: {
     name: '로그인',
-    value: '/member/login',
+    value: buildURI('/member/login'),
     isProtected: false,
     hasBackButton: true,
   },
   CALLBACK: {
     name: '소셜 로그인 콜백',
-    value: (provider: string) => `/member/${provider}/callback`,
+    value: (provider: string) => buildURI(`/member/${provider}/callback`),
     dynamicPath: '/member/[provider]/callback',
     isProtected: false,
     hasBackButton: false,
   },
   SIGNUP: {
     name: '회원가입',
-    value: '/member/signup',
+    value: buildURI('/member/signup'),
     isProtected: false,
     hasBackButton: true,
   },
   PROFILE: {
     MYPAGE: {
       name: '마이페이지',
-      value: '/member/profile/mypage',
+      value: buildURI('/member/profile/mypage'),
       isProtected: true,
       hasBackButton: false,
     },
-
     MODIFY: {
       name: '프로필 수정',
-      value: '/member/profile/modify',
+      value: buildURI('/member/profile/modify'),
       isProtected: true,
       hasBackButton: true,
     },
     BADGE: {
       name: '뱃지 조회',
-      value: '/member/profile/badge',
+      value: buildURI('/member/profile/badge'),
       isProtected: true,
       hasBackButton: true,
     },
   },
   ALARM: {
     name: '알림 확인',
-    value: '/member/alarm',
+    value: buildURI('/member/alarm'),
     isProtected: true,
     hasBackButton: true,
   },
   CHALLENGE: {
     PARTICIPATE: {
-      // 참여 중인 챌린지
       LIST: {
         name: '참여중인 챌린지',
-        value: `/member/challenge/participate/list`,
+        value: (status?: ChallengeStatus) => buildURI('/member/challenge/participate/list', { query: { status } }),
         isProtected: true,
         hasBackButton: false,
       },
     },
     VERIFICATION: {
-      // 특정 챌린지 인증 현황
       STATUS: {
         name: '특정 챌린지 인증 현황',
-        value: (challengeId: number) => `/member/challenge/${challengeId}/verification/status`,
+        value: (challengeId: number) => buildURI(`/member/challenge/${challengeId}/verification/status`),
         isProtected: true,
         hasBackButton: true,
       },
     },
-
     CREATE: {
-      // 생성한 챌린지
       LIST: {
         name: '생성한 챌린지',
-        value: '/member/challenge/create/list',
+        value: buildURI('/member/challenge/create/list'),
         isProtected: true,
         hasBackButton: true,
       },
@@ -167,7 +162,7 @@ const MEMBER_URL = {
   STORE: {
     PURCHASED: {
       name: '나뭇잎 상점 구매 목록',
-      value: '/member/store/list',
+      value: buildURI('/member/store/list'),
       isProtected: true,
       hasBackButton: true,
     },
@@ -177,7 +172,7 @@ const MEMBER_URL = {
 const STORE_URL = {
   INDEX: {
     name: '나뭇잎 상점',
-    value: `/store`,
+    value: buildURI('/store'),
     isProtected: false,
     hasBackButton: false,
   },
