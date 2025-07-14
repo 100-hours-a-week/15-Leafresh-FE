e9d4224ece5febaab7f795cfc2045ee8f0aac0a8
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2025-07-14 21:34:03 +0900

    release : v2.2.1 (#387)
    * feat : 타임딜 시작 가이드라인 제공 (#364)
    
    * refactor: 드롭다운 중복 네이밍 변경
    
    * refactor: 기존 OngoingTimeDealList Props명 변경
    
    * feat: 타임딜 시작 전 로그인 상태 확인 후 모달 및 시작 전 토스트 표시
    
    * PR:[fix] 챗봇 답변 독립 (#368)
    
    * feat: 메서드 종속 제거, 독립 상태로 렌더
    
    * feat: api 로딩 상태 추가
    
    * feat(CL): Prod CI/CD 워크플로우 업데이트 (#379)
    
    * prod용 deploy cicd 수정
    
    * 브랜치 deploy 조건에 브랜치 추가
    
    * Updated FE PROD CI/CD workflow
    
    * Fix YAML syntax error on line 185
    
    * Uncomment build-prod job for artifact deployment
    
    ---------
    
    Co-authored-by: jchanho99 <116345531+jchanho99@users.noreply.github.com>
    
    * fix:  SSR 이후 CSR에서 불필요한 refetch 제거 (#378)
    
    * feat: SSR refetch 방지를 위한 staleTime > 0 설정
    
    * chore: 이전 refetch 방지 임시 설정 삭제
    
    * feat: 서버 GC를 위한 gcTime : Infinity 설정
    
    * refactor: 코드 순서 변경
    
    * feat: 단체 챌린지 참여 후, "인증 대기"로 라우팅 되도록 변경 (#376)
    
    * chore: 인증 종료탭의 상황에 따른 버튼 텍스트 변경
    
    * feat: URI 생성시 query + fragment 빌드 코드 생성 및 적용
    
    * feat: 인증 페이지 라우트 방식에 status 추가 및 챌린지 참여시 not_started로 이동
    
    * fix: prod ci/cd 변경 및 Dockerfile 명시적 파일 복사 (#382)
    
    * Update fe-cicd.yml
    
    * Update fe-cicd.yml
    
    * Update Dockerfile: next.config.ts 명시적으로 추가
    
    * fix: 서버 환경에서 gcTime 기본 설정 적용되도록 순서 변경 (#383)
    
    * fix: CI/CD 실행 안되는 문제 해결 (#384)
    
    * Update fe-cicd.yml: 실행 안되는 문제 해결
    
    * Update: notify를 merge됐을 때만 실행되게 변경
    
    * Fix: CI/CD 중 lint, unit 테스트가 merge 이후에 실행되지 않도록 변경 (#385)
    
    * Update: dev 및 prod 별 디스코드 알람 별도 적용 (#386)
    
    ---------
    
    Co-authored-by: sunsetdust <115306845+sunsetdust@users.noreply.github.com>
    Co-authored-by: pieceofizzy <jaiunreve2@gmail.com>
    Co-authored-by: jchanho99 <116345531+jchanho99@users.noreply.github.com>

--- src/app/(with-layout)/(with-header)/page.tsx
+++ src/app/(with-layout)/(with-header)/page.tsx
@@ -18,22 +18,25 @@ const Page = async () => {
     const dayOfWeek: DayType = getDayOfWeek(now)
 
     await Promise.all([
-      queryClient.prefetchQuery({
-        queryKey: QUERY_KEYS.CHALLENGE.GROUP.CATEGORIES,
-        queryFn: getGroupChallengeCategoryList,
-        ...QUERY_OPTIONS.CHALLENGE.GROUP.CATEGORIES,
-      }),
-
+      // 이벤트 챌린지 목록
       queryClient.prefetchQuery({
         queryKey: QUERY_KEYS.CHALLENGE.EVENT.LIST,
         queryFn: getEventChallengeList,
         ...QUERY_OPTIONS.CHALLENGE.EVENT.LIST,
       }),
 
+      // 개인 챌린지 목록
       queryClient.prefetchQuery({
         queryKey: QUERY_KEYS.CHALLENGE.PERSONAL.LIST(dayOfWeek),
         queryFn: () => getPersonalChallengeList({ dayOfWeek }),
         ...QUERY_OPTIONS.CHALLENGE.PERSONAL.LIST,
+      }),
+
+      // 단체 챌린지 카테고리 목록
+      queryClient.prefetchQuery({
+        queryKey: QUERY_KEYS.CHALLENGE.GROUP.CATEGORIES,
+        queryFn: getGroupChallengeCategoryList,
+        ...QUERY_OPTIONS.CHALLENGE.GROUP.CATEGORIES,
       }),
     ])
 
