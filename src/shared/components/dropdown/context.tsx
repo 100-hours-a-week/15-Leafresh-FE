b065813304a6d016d0d9ae90a45aaff678ef627e
jihostudy <cale.jiho@gmail.com>
2025-07-19 23:23:35 +0900

    refactor: 드롭다운 상태 타입 any -> unknown 구체화
    + 미사용 제너릭 타입 T 삭제

--- src/shared/components/dropdown/context.tsx
+++ src/shared/components/dropdown/context.tsx
@@ -3,19 +3,17 @@ import { createContext, useContext } from 'react'
 /**
  * 드롭다운 내부에서 관리하는 값/함수
  */
-interface DropdownContextType<T, F> {
+interface DropdownContextType<F> {
   onSelect: F
 
   isOpen: boolean
   toggle: () => void
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const DropdownContext = createContext<DropdownContextType<any, any> | null>(null)
+export const DropdownContext = createContext<DropdownContextType<unknown> | null>(null)
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function useDropdownContext<T, F extends (...args: any[]) => any>() {
+export function useDropdownContext<F extends (...args: unknown[]) => unknown>() {
   const context = useContext(DropdownContext)
   if (!context) throw new Error('Dropdown compound component cannot be rendered outside Dropdown')
-  return context as DropdownContextType<T, F>
+  return context as DropdownContextType<F>
 }
