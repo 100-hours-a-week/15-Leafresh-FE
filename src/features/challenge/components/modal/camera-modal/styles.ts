8e55565d492eba1cd12e424a4a414310488a0d0b
jihostudy <cale.jiho@gmail.com>
2025-07-29 14:32:02 +0900

    fix: 모바일에서의 카메라 좌우반전 해제

--- src/features/challenge/components/modal/camera-modal/styles.ts
+++ src/features/challenge/components/modal/camera-modal/styles.ts
@@ -71,12 +71,22 @@ export const CameraWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.lfInputBackground.base};
 `
 
-export const CameraView = styled.video<{ facingMode: FacingMode }>`
+// export const CameraView = styled.video<{ facingMode: FacingMode }>`
+//   height: 100%;
+//   aspect-ratio: 1/1;
+//   object-fit: cover;
+
+//   transform: ${({ facingMode }) => (facingMode === 'environment' ? 'scaleX(-1)' : 'scaleX(1)')};
+// `
+export const CameraView = styled.video<{
+  facingMode: FacingMode
+  isMobile: boolean
+}>`
   height: 100%;
   aspect-ratio: 1/1;
   object-fit: cover;
 
-  transform: ${({ facingMode }) => (facingMode === 'environment' ? 'scaleX(-1)' : 'scaleX(1)')};
+  transform: ${({ facingMode, isMobile }) => (facingMode === 'environment' && !isMobile ? 'scaleX(-1)' : 'scaleX(1)')};
 `
 
 export const ImagePreview = styled(Image)`
