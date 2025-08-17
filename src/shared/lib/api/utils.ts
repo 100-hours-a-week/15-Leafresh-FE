f480e2f818e9cfa4a79d7fa5ccda787579ca4158
JiHo00 <110150963+jihostudy@users.noreply.github.com>
2025-08-17 14:20:10 +0900

    feat: V3 AWS API 환경 설정 및 도메인 변경 (#453)
    * feat: AWS Prod 환경 API 수정
    
    * feat: 로컬 개발환경 도메인 수정
    
    /etc/hosts도 같이 수정되었음

--- src/shared/lib/api/utils.ts
+++ src/shared/lib/api/utils.ts
@@ -6,15 +6,14 @@ export const getClientFetchOrigin = () => {
 
   switch (runtimeEnv) {
     case 'local':
-      // return 'https://dev-leafresh.app/api/next/proxy' // Dev FE 라우트
-      // return 'https://fe.dev-leafresh.app/api/next/proxy'
-      // return 'https://springboot.dev-leafresh.app' // TODO: 현재는 백엔드 임시 개방 -> 추후 프론트엔드 프록시로 연결
-      return 'https://leafresh.click'
-    case 'dev':
-      return 'https://dev-leafresh.app/api/next/proxy' // Dev FE 라우트
+      // return 'https://dev-leafresh.app/api/next/proxy' // V1
+      // return 'https://springboot.dev-leafresh.app' // V2
+      // return 'https://fe.dev-leafresh.app/api/next/proxy' // V2
+      return 'https://leafresh.app'
+    // case 'dev':
+    //   return 'https://dev-leafresh.app/api/next/proxy' // V2
     case 'prod':
-      // return 'https://leafresh.app' // 로드밸런서
-      return 'https://leafresh.click'
+      return 'https://leafresh.app'
     default:
       throw new Error(`Unknown Client NEXT_PUBLIC_RUNTIME: ${runtimeEnv}`)
   }
@@ -28,16 +27,14 @@ export const getServerFetchOrigin = () => {
 
   switch (runtimeEnv) {
     case 'local':
-      // return 'https://dev-leafresh.app/api/next/proxy' // Dev FE 라우트
-      // return 'https://springboot.dev-leafresh.app'
-      return 'https://leafresh.click'
-    case 'dev':
-      // return '35.216.18.52' //외부망으로 임시 연결
-      // return 'http://10.0.1.67:8080' // 내부망
-      return 'http://backend-service.default.svc.cluster.local:80'
+      // return 'https://springboot.dev-leafresh.app' // V2
+      // return 'https://dev-leafresh.app/api/next/proxy' // V2
+      return 'https://leafresh.app' // V3
+    // case 'dev':
+    // return 'http://10.0.1.67:8080' // V2
+    // return 'http://backend-service.default.svc.cluster.local:80' // V3
     case 'prod':
-      // return 'https://leafresh.app' // 로드밸런서
-      return 'https://leafresh.click'
+      return 'https://leafresh.app' // V3
     default:
       throw new Error(`Unknown Server NEXT_PUBLIC_RUNTIME: ${runtimeEnv}`)
   }
